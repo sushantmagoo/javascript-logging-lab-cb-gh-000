36a5d70e6e94fb8a7c9b755ae749ee82aa513368
Sushant Magoo <sushantmagoo@gmail.com>
2017-07-30 08:30:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+// First test
+console.error("This is my first error logging")
+
+// Second test
+console.log("This is my first logging")
+
+// Third test
+console.warn("This is my first warning log")
